7dfcaf29e1f31138e2874e5fcacf5e91af5209b1
Adhithiya <adhithiya.g2023ai-ds@sece.ac.in>
2024-06-05 21:27:52 +0530

    first commit

---
+++ career_recommend/testapp.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def career():
+    return render_template("form.html")
+
+@app.route('/predict', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        
+        # Convert form data to a list of floats
+        arr = [float(value) for value in result.values()]
+        data = np.array(arr).reshape(1, -1)
+        print(data)
+        
+        # Load the saved model
+        loaded_model = pickle.load(open("careerlast.pkl", 'rb'))
+        
+        # Make predictions
+        predictions = loaded_model.predict(data)
+        print(predictions)
+        
+        # Get probabilities
+        pred_proba = loaded_model.predict_proba(data)
+        print(pred_proba)
+        
+        pred = pred_proba > 0.05
+        
+        i, j, index = 0, 0, 0
+        res, final_res = {}, {}
+        
+        while j < 17:
+            if pred[i, j]:
+                res[index] = j
+                index += 1
+            j += 1
+        
+        index = 0
+        for key, values in res.items():
+            if values != predictions[0]:
+                final_res[index] = values
+                print('final_res[index]:', final_res[index])
+                index += 1
+        
+        jobs_dict = {
+            0: 'AI ML Specialist',
+            1: 'API Integration Specialist',
+            2: 'Application Support Engineer',
+            3: 'Business Analyst',
+            4: 'Customer Service Executive',
+            5: 'Cyber Security Specialist',
+            6: 'Data Scientist',
+            7: 'Database Administrator',
+            8: 'Graphics Designer',
+            9: 'Hardware Engineer',
+            10: 'Helpdesk Engineer',
+            11: 'Information Security Specialist',
+            12: 'Networking Engineer',
+            13: 'Project Manager',
+            14: 'Software Developer',
+            15: 'Software Tester',
+            16: 'Technical Writer'
+        }
+        
+        data1 = predictions[0]
+        print(data1)
+        
+        return render_template("result.html", final_res=final_res, job_dict=jobs_dict, job0=data1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
